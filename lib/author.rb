c1a9fa4ef1f8ecd002b4482994c680e6a25ada61
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-29 04:36:55 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+class Author 
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def posts 
+    Post.all.select {|post| post.author.name == self.name}
+  end
+  
+  def add_post
+    
+  end
+end
